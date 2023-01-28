8bb088fa7271b7dfb2eeac6e7b29679972481d1d
Luigi311 <git@luigi311.com>
2023-01-27 23:37:03 -0700

    Update build.sh for arch tags

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ docker buildx use multiarch
 # Get the current date and use it to generate a date-based tag
 TAG=$(date +"%Y-%m-%d")
 
-docker buildx build --platform "$1" -t "luigi311/low-power-image-processing-base-image:$TAG" -t "luigi311/low-power-image-processing-base-image:latest" --push .
+docker buildx build --platform "linux/${1}" -t "luigi311/low-power-image-processing-base-image:${TAG}-${1}" -t "luigi311/low-power-image-processing-base-image:latest-${1}" --push .
